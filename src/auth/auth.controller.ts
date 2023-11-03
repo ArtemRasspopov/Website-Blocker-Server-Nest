81ab8b8377b89b6d420cea3880bc3daaf31b8ca5
Artem Rasspopov <Artem199833@gmail.com>
2023-11-03 14:51:36 +0300

    auth is ready

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,74 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { GetSessionInfoDto, SignInBodyDto, SignUpBodyDto } from './dto';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { Response } from 'express';
+import { CookieService } from './cookie.service';
+import { AuthGuard } from './auth.guard';
+import { sessionInfo } from './session-info.decorator';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private cookiService: CookieService,
+  ) {}
+
+  // Регистрация
+  @Post('sign-up')
+  @ApiCreatedResponse()
+  async signUp(
+    @Body() body: SignUpBodyDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { accessToken } = await this.authService.signUp(
+      body.email,
+      body.password,
+    );
+
+    this.cookiService.setToken(res, accessToken);
+  }
+
+  // Авторизация
+  @Post('sign-in')
+  @ApiOkResponse()
+  @HttpCode(HttpStatus.OK)
+  async signIn(
+    @Body() body: SignInBodyDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { accessToken } = await this.authService.signIn(
+      body.email,
+      body.password,
+    );
+
+    this.cookiService.setToken(res, accessToken);
+  }
+
+  // Логаут
+  @Post('sign-out')
+  @ApiOkResponse()
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard)
+  signOut(@Res({ passthrough: true }) res: Response) {
+    this.cookiService.removeToken(res);
+  }
+
+  @Get('session')
+  @ApiOkResponse({
+    type: GetSessionInfoDto,
+  })
+  @UseGuards(AuthGuard)
+  getSessionInfo(@sessionInfo() session: GetSessionInfoDto) {
+    return session;
+  }
+}
