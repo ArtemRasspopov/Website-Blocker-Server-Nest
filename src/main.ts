66a0064764bb6529614c660f31071e17c54a097b
Artem Rasspopov <Artem199833@gmail.com>
2023-11-17 16:25:18 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,17 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cors({
+      origin: 'http://localhost:3001', // Замените на свой фронтенд URL
+      credentials: true,
+    }),
+  );
 
   const config = new DocumentBuilder().setTitle('Blocker').build();
 
